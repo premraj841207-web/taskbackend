130145ca479d559acc7f3d4157774d38be910012
premraj841207-web <premraj841207@gmail.com>
2026-02-02 01:19:03 +0530

    Initial backend setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const taskRoutes = require("./routes/taskRoutes");
 const errorHandler = require("./middlewares/errorHandler");
@@ -10,33 +11,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB (Vercel-safe)
-let isConnected = false;
-
-const connectDB = async () => {
-  if (isConnected) return;
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI not defined");
-  }
-  await mongoose.connect(process.env.MONGO_URI);
-  isConnected = true;
-};
-
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Task API running 🚀",
-    endpoints: {
-      tasks: "/tasks",
-    },
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
   });
-});
 
 app.use("/tasks", taskRoutes);
+
 app.use(errorHandler);
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
 
 module.exports = app;
