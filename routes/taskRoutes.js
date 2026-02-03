e52f8454a94d71c67b42310eb569f1a8312d7c42
Ritik841207 <devilraj11011@gmail.com>
2026-02-03 23:10:23 +0530

    Initial backend setup

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,29 +1,4 @@
-// const express = require("express");
-// const router = express.Router();
 
-// const {
-//   getTasks,
-//   addTask,
-//   toggleTask,
-//   deleteTask,
-// } = require("../controllers/taskController");
-
-// const { getDailyAnalysis ,getLast28DaysAnalysis} = require("../controllers/taskController");
-
-// // 📥 Get all tasks
-// router.get("/", getTasks);
-
-// // ➕ Add new task
-// router.post("/", addTask);
-
-// // 🔁 Toggle task complete
-// router.put("/:id", toggleTask);
-
-// // 🗑️ Delete task
-// router.delete("/:id", deleteTask);
-// router.get("/analysis", getDailyAnalysis);
-// router.get("/analysis/28days", getLast28DaysAnalysis);
-// module.exports = router;
 
 
 const express = require("express");
